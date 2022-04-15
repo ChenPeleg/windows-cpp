61ae9b8c7e5b14b87ba0518a190e793f2761407a
chenp <chenpeleg@gmail.com>
2022-04-15 14:59:48 +0300

    works getting info from console

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,8 @@ int main()
     int radius{};
     int respons;
     int e = constants::eight();
+    printf("\033c");
+    std::cout << "\n\n\n";
     std::cout << "Hello World! eight is 1 " << e << std::endl;
     std::cout << "answer one question " << std::endl;
     std::cin >> respons;
