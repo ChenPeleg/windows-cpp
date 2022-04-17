49a4385cd880d318876d3020e4deab3f6805f343
chenp <chenpeleg@gmail.com>
2022-04-17 16:25:04 +0300

    added exit to the console

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,23 @@
 #include "constants.h"
 #include "grapihics_utils.h"
 #include <windows.h>
+#include <iomanip>
 #include <iostream>
+
+const size_t BUFFER_SIZE = 1024;
+bool checkExit(char *stringToCheck)
+{
+    char exitString[5] = "exit";
+    bool isExit = true;
+    for (int i = 0; i < 5; i++)
+    {
+        if (stringToCheck[i] != exitString[i])
+        {
+            isExit = false;
+        }
+    }
+    return isExit;
+}
 
 int main()
 {
@@ -9,13 +25,27 @@ int main()
     int respons;
     int e = constants::eight();
 
+    // char inputFileName[BUFFER_SIZE];
+    char *inputFileName = new char[BUFFER_SIZE];
+
     graphicUtils::clear();
-    for (int i = 1; i < 3; i++)
+    for (int i = 1; i < 10; i++)
     {
+        graphicUtils::clear();
         std::cout << "\n\n\n";
         std::cout << "Hello World! eight is 1 " << e << std::endl;
+        std::cout << inputFileName << std::endl;
+        std::cout << inputFileName << std::endl;
+        std::cout << inputFileName << std::endl;
         std::cout << "answer one question " << std::endl;
-        std::cin >> respons;
-        graphicUtils::clear();
+        std::cin >> std::setw(BUFFER_SIZE) >> inputFileName;
+        if (checkExit(inputFileName))
+        {
+            std::cout << "Goodbye!" << std::endl;
+            std::cin >> std::setw(BUFFER_SIZE) >> inputFileName;
+            i = 20;
+            break;
+        }
     }
+    delete[] inputFileName;
 }
