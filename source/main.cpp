8e734722054659f377b287177bc86dbd4024991a
chenp <chenpeleg@gmail.com>
2022-04-19 14:26:26 +0300

    experiments with event loop listening to a key

--- source/main.cpp
+++ source/main.cpp
@@ -4,8 +4,12 @@
 #include <windows.h>
 #include <iomanip>
 #include <iostream>
+#include <conio.h>
 
 const size_t BUFFER_SIZE = 1024;
+int eventLoop();
+int eventLoop2();
+
 bool checkExit(char *stringToCheck)
 {
     char exitString[5] = "exit";
@@ -19,8 +23,14 @@ bool checkExit(char *stringToCheck)
     }
     return isExit;
 }
-
 int main()
+{
+    graphicUtils::clear();
+    eventLoop2();
+    return 0;
+}
+int old_main()
+
 {
     int radius{};
     int respons;
@@ -28,15 +38,17 @@ int main()
     char *inputFileName = new char[BUFFER_SIZE];
 
     graphicUtils::clear();
-    for (int i = 1; i < 100; i++)
+    for (int i = 1; i < 1; i++)
     {
         graphicUtils::clear();
-        std::cout << "\n\n\n";
-        std::cout << "Hello World! is 1 " << images::image1 << std::endl;
+        std::cout << "\n\n";
+        std::cout << "Dungeons and Ghosts" << images::image1 << std::endl;
+        std::cout << inputFileName << std::endl;
         std::cout << inputFileName << std::endl;
         std::cout << inputFileName << std::endl;
         std::cout << inputFileName << std::endl;
         std::cout << "answer one question " << std::endl;
+
         std::cin >> std::setw(BUFFER_SIZE) >> inputFileName;
         if (checkExit(inputFileName))
         {
@@ -46,4 +58,39 @@ int main()
         }
     }
     delete[] inputFileName;
+    return 0;
+}
+
+int keypressed(int key)
+{
+    std::cout << key << std::endl;
+    return 0;
+}
+int eventLoop()
+{
+    int c, l, last = 0;
+    char str[100];
+    do
+    {
+        c = getchar();
+        str[l] = c;
+        if (c != l)
+        {
+            keypressed(c);
+        }
+
+    } while (c != '\n');
+    return 0;
+}
+int eventLoop2()
+{
+    char a;
+    do
+    {
+        a = getche();
+        graphicUtils::clear();
+        std::cout << a;
+
+    } while (a != '\n');
+    return 0;
 }
