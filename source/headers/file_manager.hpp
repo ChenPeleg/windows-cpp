63fddd7e828c2531c397fb6ac286a8372077323f
chenp <chenpeleg@gmail.com>
2022-06-12 14:50:08 +0300

    able to get file size

--- source/headers/file_manager.hpp
+++ source/headers/file_manager.hpp
@@ -11,20 +11,50 @@ class FileManager
 private:
     /* data */
     int FileDataSize;
+    const char *FileName;
+    long getFileLength(fstream &fileRef);
 
 public:
     FileManager(const char *fileName = "data/savefile.dat", int fileDataSize = 100);
-
     ~FileManager();
+    void add(const char *textToWrite);
+    long GetFileLength();
+    fstream openFile();
 };
 
-FileManager::FileManager(const char *fileName, int fileDataSize) : FileDataSize(fileDataSize)
+/*** Class ***/
+
+FileManager::FileManager(const char *fileName, int fileDataSize) : FileDataSize(fileDataSize), FileName(fileName){
+                                                                                                   // fstream file = openFile();// file.close();
+                                                                                               };
+long FileManager::getFileLength(fstream &fileRef)
 {
-    ofstream outfile;
-    outfile.open(fileName);
-    // outfile << "abc123" << endl;
-    outfile.close();
-};
+
+    fileRef.seekg(0, fileRef.end);
+    return fileRef.tellg();
+}
+long FileManager::GetFileLength()
+{
+    long len = 0;
+    fstream file = openFile();
+    len = getFileLength(file);
+    file.close();
+    return len;
+}
+fstream FileManager::FileManager::openFile()
+{
+
+    fstream file;
+    file.open(FileName);
+    return file;
+}
+void FileManager::add(const char *textToWrite)
+{
+
+    fstream file = openFile();
+    file << textToWrite;
+    file.close();
+}
 
 FileManager::~FileManager()
 {
